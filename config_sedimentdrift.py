f4aaa0c8fe4229fea038d1b3aa6211bcbbbebb25
Trond Kristiansen <tk@raskdev.com>
2021-03-31 10:55:55 -0700

    Changed to limit number of files read into memory

--- config_sedimentdrift.py
+++ config_sedimentdrift.py
@@ -42,7 +42,7 @@ class MartiniConf():
         # Glomma - seed locations
         self.st_lons = [10.962920]
         self.st_lats = [59.169194]
-        self.number_of_particles = 200
+        self.number_of_particles = 100
         self.release_radius = 300
         # diameter in meter, densities in kg/m3
 
